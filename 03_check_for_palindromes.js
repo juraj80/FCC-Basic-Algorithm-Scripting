68da4463f9533485eeb14f3cf105e71c185629c6
Juraj <juraj.klucka@gmail.com>
2016-07-14 15:16:05 +0200

    Update 03_check_for_palindromes.js

--- 03_check_for_palindromes.js
+++ 03_check_for_palindromes.js
@@ -1,9 +1,19 @@
 function palindrome(str) {
-  // Good luck!
-  return str.toLowerCase().replace(/[^0-9a-z]/gi, '');
+
+ str=str.replace(/[^0-9a-z]/gi,"").toLowerCase();
+ var len=str.length;
+  
+  for (var i=0;i<len/2;i++){
+  
+    if(str[i]!==str[len-1-i]){
+      return false;
+    }
+   }
+   return true;
 }
 
-
-
-palindrome("My age is 0, 0 si ega ym.");
-
+/* //another way
+  var arr_reversed=arr.split("").reverse().join("");
+return arr===arr_reversed;
+*/
+palindrome("almostomla");
